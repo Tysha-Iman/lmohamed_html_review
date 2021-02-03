066cf6d514f1922c70cb39ea5b5fd2d88ec13bb4
Tysha-Iman <78460127+Tysha-Iman@users.noreply.github.com>
2021-02-03 03:49:01 -0700

    All Done!

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,45 @@ var $two = $("#j1_p");
 var $three = $("#j2");
 var $four = $("#j3");
 var $five = $("#j3_p");
+var $boxes = $(".box");
 
 
 $one.on("click", function(){
 	$(this).hide();
-	$two.show();})
+	$two.show();
+	$boxes.css("background-color","#CD5C5C");
+	$boxes.css("color","white");
+})
+
 $two.on("click", function(){
 	$(this).hide();
-	$three.show();})
+	$three.show();
+	$boxes.css("background-color","#ACD07A");
+
+})
+
 $three.on("click", function(){
 	$(this).hide();
-	$four.show();})
+	$four.show();
+	$boxes.css("background-color","#E6CEE7");
+	$boxes.css("color","#F08080");
+})
+
 $four.on("click", function(){
 	$(this).hide();
-	$five.show();})
+	$five.show();
+	$boxes.css("background-color","#947995");
+	$boxes.css("color","white");
+})
+
 $five.on("click", function(){
 	$(this).hide();
-	$one.show();})
+	$one.show();
+	$boxes.css("background-color","#FFF5EF");
+	$boxes.css("color","#F08080");
+})
+
 $one.on("click", function(){
 	$(this).hide();
-	$two.show();})
+	$two.show();
+})
